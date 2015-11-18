a077dba1dbc665183697064944093eb0fdf947d7
Alexander Zaguzin <st7105@gmail.com>
2015-11-18 12:12:45 +0600

    Use IN_CLOSE_WRITE instead of IN_CREATE for inotify, disable Dart, update gitignore

--- include/inotify/dir_monitor_impl.hpp
+++ include/inotify/dir_monitor_impl.hpp
@@ -43,7 +43,7 @@ public:
 
     void add_directory(const std::string &dirname)
     {
-        int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO);
+        int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO | IN_CLOSE_WRITE);
         if (wd == -1)
         {
             boost::system::system_error e(boost::system::error_code(errno, boost::system::get_system_category()), "boost::asio::dir_monitor_impl::add_directory: inotify_add_watch failed");
@@ -160,12 +160,12 @@ private:
                 dir_monitor_event::event_type type = dir_monitor_event::null;
                 switch (iev->mask)
                 {
-                case IN_CREATE: type = dir_monitor_event::added; break;
+                case IN_CLOSE_WRITE: type = dir_monitor_event::added; break;
                 case IN_DELETE: type = dir_monitor_event::removed; break;
                 case IN_MODIFY: type = dir_monitor_event::modified; break;
                 case IN_MOVED_FROM: type = dir_monitor_event::renamed_old_name; break;
                 case IN_MOVED_TO: type = dir_monitor_event::renamed_new_name; break;
-                case IN_CREATE | IN_ISDIR:
+                case IN_CLOSE_WRITE | IN_ISDIR:
                     {
                         type = dir_monitor_event::added;
                         add_directory(get_dirname(iev->wd) + "/" + iev->name);
